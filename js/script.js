b750dffa30e1739e59b52776465f9104e63c9f90
cassijobs <41442597+cassijobs@users.noreply.github.com>
2026-07-06 12:59:12 -0300

    Update encounters array to use JORNADA1

--- js/script.js
+++ js/script.js
@@ -8,29 +8,39 @@ SCRIPT PRINCIPAL
 window.addEventListener("load", iniciar);
 
 async function iniciar() {
+
     const parametros = new URLSearchParams(window.location.search);
 
     if (parametros.get("reset") === "1") {
+
         MEMORIA.resetar();
+
     } else {
+
         MEMORIA.carregar();
+
     }
 
-    const encontros = [
-        PRIMEIRO_ENCONTRO,
-        SEGUNDO_ENCONTRO,
-        TERCEIRO_ENCONTRO
-    ];
+    const encontros = JORNADA1;
 
     let indice = MEMORIA.encontroAtual - 1;
 
     if (!MEMORIA.nome) {
+
         indice = 0;
+
     }
 
     if (indice < 0 || indice >= encontros.length) {
+
         indice = encontros.length - 1;
+
     }
 
-    await Condutor.executar(encontros[indice]);
+    await Condutor.executar(
+
+        encontros[indice]
+
+    );
+
 }
