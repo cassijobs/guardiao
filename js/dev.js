65152066d1d1b9bb4199b3bb5f187e421cfafd38
cassijobs <41442597+cassijobs@users.noreply.github.com>
2026-07-07 12:51:16 -0300

    Add developer mode configuration in dev.js

---
+++ js/dev.js
@@ -0,0 +1,18 @@
+/*
+======================================================
+GUARDIÃO v3.0
+MODO DESENVOLVEDOR
+======================================================
+*/
+
+const DEV = {
+
+    ativo: true,
+
+    encontro: 7,
+
+    nome: "Cassiano",
+
+    ignorarTravaDiaria: true
+
+};
