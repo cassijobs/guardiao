ac5bcfa22ef9207a7ffde1fe739d727659639c70
cassijobs <41442597+cassijobs@users.noreply.github.com>
2026-07-07 12:42:57 -0300

    Add nomeTeste property to config.js

--- js/config.js
+++ js/config.js
@@ -14,6 +14,7 @@ const CONFIG = {
     // Escolha aqui qual encontro abrir no modo desenvolvedor.
     // Exemplo: 1, 2, 3, 4...
     encontroTeste: 3,
+     nomeTeste: "Cassiano",
 
     versao: "2.0.1",
 
